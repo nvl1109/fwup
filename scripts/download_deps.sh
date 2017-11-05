5f4911b3a987841ddb6d6db3c92ca25e0b247404
Frank Hunleth <fhunleth@troodon-software.com>
2017-11-04 21:47:40 -0400

    Use zlib-ng for performance improvements
    zlib-ng pulls in many zlib patches used in Chromium and other places to
    improve performance. The claims are fairly significant (18%-30%) for
    decoding png files. Since firmware archives are much larger than pngs,
    one would expect similar or better improvements. Note that this only
    helps platforms that the zlib-ng developers have chosen to optimize.
    This primarily affects x86 and to a lesser extent ARM.

--- scripts/download_deps.sh
+++ scripts/download_deps.sh
@@ -13,9 +13,43 @@ source $BASE_DIR/scripts/common.sh
 # Initialize some directories
 mkdir -p $DOWNLOAD_DIR
 
-cd $DOWNLOAD_DIR
-[ -e zlib-$ZLIB_VERSION.tar.xz ] || curl -LO http://zlib.net/zlib-$ZLIB_VERSION.tar.xz
-[ -e confuse-$CONFUSE_VERSION.tar.xz ] || curl -LO https://github.com/martinh/libconfuse/releases/download/v$CONFUSE_VERSION/confuse-$CONFUSE_VERSION.tar.xz
-[ -e libarchive-$LIBARCHIVE_VERSION.tar.gz ] || curl -LO http://libarchive.org/downloads/libarchive-$LIBARCHIVE_VERSION.tar.gz
-[ -e libsodium-$LIBSODIUM_VERSION.tar.gz ] || curl -LO https://github.com/jedisct1/libsodium/releases/download/$LIBSODIUM_VERSION/libsodium-$LIBSODIUM_VERSION.tar.gz
+download()
+{
+    archive=$DOWNLOAD_DIR/$1
+    url=$2
+
+    if [[ ! -e $archive ]]; then
+        echo "Downloading $url..."
+        curl -L -o $archive $url
+    fi
+}
+
+clone()
+{
+    archive=$DOWNLOAD_DIR/$1
+    url=$2
+    tag=$3
+
+    if [[ ! -e $archive ]]; then
+        echo "Cloning $url..."
+
+        tmpdir=$DOWNLOAD_DIR/tmp
+        rm -fr $tmpdir
+        mkdir -p $tmpdir
+        pushd $tmpdir
+        git clone $url
+
+        cd $(basename $url .git)
+        git checkout $tag
+        git archive --format=tar --prefix=$(basename $archive .tar.xz)/ $tag | xz -c > $archive
+        popd
+        rm -fr $tmpdir
+    fi
+}
+
+clone zlib-$ZLIB_VERSION.tar.xz https://github.com/Dead2/zlib-ng.git $ZLIB_VERSION
+#download zlib-$ZLIB_VERSION.tar.xz http://zlib.net/zlib-$ZLIB_VERSION.tar.xz
+download confuse-$CONFUSE_VERSION.tar.xz https://github.com/martinh/libconfuse/releases/download/v$CONFUSE_VERSION/confuse-$CONFUSE_VERSION.tar.xz
+download libarchive-$LIBARCHIVE_VERSION.tar.gz http://libarchive.org/downloads/libarchive-$LIBARCHIVE_VERSION.tar.gz
+download libsodium-$LIBSODIUM_VERSION.tar.gz https://github.com/jedisct1/libsodium/releases/download/$LIBSODIUM_VERSION/libsodium-$LIBSODIUM_VERSION.tar.gz
 
--- scripts/third_party_versions.sh
+++ scripts/third_party_versions.sh
@@ -4,7 +4,8 @@
 # Version numbers for the 3rd party source dependencies
 #
 
-ZLIB_VERSION=1.2.11
+# Use zlib-ng for a small performance boost on x86 and arm
+ZLIB_VERSION=e07a52dbaa35d003f5659b221b29d220c091667b
 LIBARCHIVE_VERSION=3.3.1
 LIBSODIUM_VERSION=1.0.13
 CONFUSE_VERSION=3.2.1
